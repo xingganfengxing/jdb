d4185fbc0a9e9da4c3e12bd4d84c8fddca98eb00
liufengking <liufeng_king@126.com>
2015-08-22 14:06:54 +0800

    管理员管理、焦点管理、举报管理

---
+++ src/main/java/com/ezb/jdb/controller/pc/admin/MInformController.java
@@ -0,0 +1,70 @@
+package com.ezb.jdb.controller.pc.admin;
+
+import com.ezb.jdb.common.PageResult;
+import com.ezb.jdb.common.ResponseData;
+import com.ezb.jdb.model.Inform;
+import com.ezb.jdb.service.IInformService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * 举报管理
+ * author : liufeng
+ * create time:2015/8/22 9:47
+ */
+@Controller
+public class MInformController {
+
+    @Resource
+    private IInformService informServiceImpl;
+
+    /**
+     * @param pageResult 分页信息
+     * @param realname   检举人
+     * @param startTime  开始时间
+     * @param endTime    结束时间
+     * @param reason     举报原因
+     * @param state      处理状态
+     * @return
+     */
+    @RequestMapping(value = "pc/admin/inform/query", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    String query(
+            PageResult<Inform> pageResult,
+            String realname,
+            String startTime,
+            String endTime,
+            String reason,
+            String state) {
+
+        pageResult = informServiceImpl.query(
+                pageResult,realname,startTime,
+                endTime,reason,state);
+
+        return ResponseData.getResData(pageResult);
+    }
+
+    @RequestMapping(value = "pc/admin/inform/del", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    String del(String ids){
+        return informServiceImpl.del(ids);
+    }
+
+    /**
+     * 处理举报
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "pc/admin/inform/handle", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    String handle(String id){
+        return informServiceImpl.handle(id);
+    }
+}
