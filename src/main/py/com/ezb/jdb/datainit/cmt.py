9ac73dab2628816bbafc8f25afcaed1c7fd0357d
liufengking <liufeng_king@126.com>
2015-08-27 16:12:04 +0800

    评论数据初始化bug修复

--- src/main/py/com/ezb/jdb/datainit/cmt.py
+++ src/main/py/com/ezb/jdb/datainit/cmt.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 # Author   : liufeng
 # Create   : 2015/8/13 17:00
-import initconn,time,random
+import initconn, time, random
+
 
 def initCmt(table):
     conn = initconn.getConn()
     cur = conn.cursor()
+    circleIds = [0] * 10  # 定义长度为10的数组
 
     # 先插入10条一级评论
     for i in range(10):
+        circleIds[i] = random.randint(0, 99)
         value = [
             str(i),
             "content" + str(i),
@@ -18,11 +21,16 @@ def initCmt(table):
             random.randint(0, 99),
             random.randint(0, 99)
         ]
+        if table == "t_newscmt":
+            value[5] = circleIds[i]
+        if table == "t_circmt" or table == "t_atvcmt":
+            value[4] = circleIds[i]
         cur.execute('insert into ' + table + ' values(%s,%s,%s,%s,%s,%s,null)', value)
     conn.commit()
 
     # 插入90条 子评论
     for i in range(11, 100, 1):
+        parentCmtId = random.randint(0, 9)
         value = [
             str(i),
             "content" + str(i),
@@ -30,12 +38,17 @@ def initCmt(table):
             random.randint(0, 99),
             random.randint(0, 99),
             random.randint(0, 99),
-            random.randint(0, 9)
+            parentCmtId
         ]
+        if table == "t_newscmt":
+            value[5] = circleIds[parentCmtId]
+        if table == "t_circmt" or table == "t_atvcmt":
+            value[4] = circleIds[parentCmtId]
         cur.execute('insert into ' + table + ' values(%s,%s,%s,%s,%s,%s,%s)', value)
     conn.commit()
     cur.close()
     conn.close()
+
 
 def initNewsCmt():
     """资讯评论表"""
@@ -45,6 +58,7 @@ def initNewsCmt():
 def initAtvCmt():
     """活动评论表"""
     initCmt("t_atvcmt")
+
 
 def initCirCmt():
     """圈子评论"""
